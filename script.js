0a4df82f7f8319e148affe285bb1719d7edd86fe
fatima153m <fatema.mudhafar@ced.nahrainuniv.edu.iq>
2026-07-25 03:17:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,97 @@
 document.getElementById("analyzeBtn").onclick = function () {
-    document.getElementById("result").innerHTML = "Test working ✅";
+
+    let hb = Number(document.getElementById("hb").value);
+    let mcv = Number(document.getElementById("mcv").value);
+
+    let gender = document.querySelector('input[name="gender"]:checked');
+
+    let result = document.getElementById("result");
+
+
+    if (!gender) {
+        result.innerHTML = "Please select gender";
+        return;
+    }
+
+
+    if (isNaN(hb) || isNaN(mcv)) {
+        result.innerHTML = "Please enter Hb and MCV";
+        return;
+    }
+
+
+    let anemia = false;
+    let severity = "";
+
+
+    if (gender.value === "male") {
+
+        if (hb < 13) {
+
+            anemia = true;
+
+            if (hb >= 11) {
+                severity = "Mild anemia";
+            } 
+            else if (hb >= 8) {
+                severity = "Moderate anemia";
+            } 
+            else {
+                severity = "Severe anemia";
+            }
+
+        }
+
+    } 
+    else {
+
+        if (hb < 12) {
+
+            anemia = true;
+
+            if (hb >= 11) {
+                severity = "Mild anemia";
+            } 
+            else if (hb >= 8) {
+                severity = "Moderate anemia";
+            } 
+            else {
+                severity = "Severe anemia";
+            }
+
+        }
+
+    }
+
+
+    let pattern = "";
+
+    if (mcv < 80) {
+        pattern = "Microcytic pattern";
+    }
+    else if (mcv <= 100) {
+        pattern = "Normocytic pattern";
+    }
+    else {
+        pattern = "Macrocytic pattern";
+    }
+
+
+
+    if (anemia) {
+
+        result.innerHTML =
+        "⚠️ Anemia: YES<br>" +
+        "Severity: " + severity + "<br>" +
+        "Pattern: " + pattern;
+
+    } 
+    else {
+
+        result.innerHTML =
+        "✅ Anemia: NO<br>" +
+        "Pattern: " + pattern;
+
+    }
+
 };
